ac39e66e58cd4bddf4ec6f620704b7f685b4ae99
Tarpelite <tarpelite_chan@foxmail.com>
2020-11-05 08:33:12 +0800

    update server

--- server/demo/views.py
+++ server/demo/views.py
@@ -5,14 +5,15 @@ from rest_framework.response import Response
 from django.http import HttpResponse
 import os
 import pymongo
-from py2neo import Node
+from py2neo import Graph, Node, Relationship, NodeMatcher
 # Create your views here.
 
 neo4j_path = "bolt://39.105.232.15:2002"
 mongo_path = pymongo.MongoClient("mongodb://%s:%s@39.105.232.15:2005"%("admin", "123456"))
 db = mongo_path["surveys"]
 col = db["production"]
-graph = Graph(database, user="neo4j", password="123456")
+graph = Graph(neo4j_path, user="neo4j", password="123456")
+matcher = NodeMatcher(graph)
 
 @api_view(["POST"])
 def upload(request):
@@ -26,19 +27,19 @@ def upload(request):
         address:"",
         path:[
             {
-                seq_id:0,
                 time: "YYYY-MM-DD HH:MM"
                 location: "str",
+                longtitude:127.022
+                latitude:98.022
                 contacts:["person A", "person B"],
                 detail:""
             },
             {
-                seq_id:1,
                 transportation: "",
                 detail:"".
             },
             {
-                seq_id: 2
+                ...
             }
         ]
     ]
@@ -48,12 +49,42 @@ def upload(request):
         col.insert_one(data)
         name = data["name"].strip()
         gender = data["gender"].strip()
-        phone = data["phone"]
-        age = data["age"]
-        address = data["address"]
-        path = data["path"]
+        phone = data["phone"].strip()
+        age = data["age"].strip()
+        address = data["address"].strip()
+        path = data["path"].strip()
+        
+        res = matcher.match("Patient", name=name, gender=gender, phone=phone, age=age, address=address).first()
+
+        tx = graph.begin()
+        if res is not None:
+            person_node = res
+        else:
+            person_node = Node("Patient", name=name, gender=gender, phone=phone, age=age, address=address)
+            
+            tx.create(person_node)
+
         for p in path:
             # every events
+            if 'location' in p:
+                time = p["time"]
+                location = p["location"]
+                longtitude = p["longtitude"]
+                latitude = p["latitude"]
+                contacts = p["contacts"]
+                detail = p["detail"]
+
+                for contact in contacts:
+                    contact_node = Node("Contact", name=contact)
+                    r1 = Relationship()
+
+                loc_node = Node("Event", time=time, location=location, longtitude=longtitude, latitude=latitude,detail=detail)
+
+
+
+
+
+            
             
 
 
