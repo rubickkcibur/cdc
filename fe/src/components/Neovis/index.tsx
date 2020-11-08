0db103fa0943813d1fced73a2a431d448521a4c7
Liuhancheng <cn_lhc@qq.com>
2020-11-08 13:36:52 +0800

    add vis

---
+++ fe/src/components/Neovis/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Neovis() {
+    return (
+        <div id={"vis"}>
+
+
+        </div>
+    )
+}
--- fe/src/pages/overview/index.tsx
+++ fe/src/pages/overview/index.tsx
@@ -1,16 +1,42 @@
 import { Select, Switch } from "antd"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import MainLayout from "../../components/MainLayoout/PageLayout"
 import sty from './index.module.scss'
 
 export default function PageOverview() {
   const [byWhat, setByWhat] = useState("people")
-  const urlLoc = 'http://39.105.232.15:2100/neovis.js/examples/ByLocation.html'
-  const urlPeo = 'http://39.105.232.15:2100/neovis.js/examples/ByPerson.html'
+  const draw = () => {
+    var config = {
+      container_id: "viz",
+      server_url: "bolt://123.57.0.181:7687",
+      server_user: "neo4j",
+      server_password: "123456",
+      labels: {
+        "Event": {
+          "caption": "location",
+          "size": "count"
+        }
+      },
+      relationships: {
+        "transport": {
+          "caption": "method",
+          "thickness": "count"
+        }
+      },
+      initial_cypher: "MATCH p=()-[r:transport]->() RETURN p LIMIT 25"
+    };
+    const NeoVis = require('neovis.js/dist/neovis')
+    const viz = new NeoVis.default(config)
+    viz.render();
+  }
+  useEffect(() => {
+    if (process.browser)
+      draw()
+  }, [])
 
   return <MainLayout>
     <div className={sty.FrameContainer}>
-      <iframe src={byWhat === "people" ? urlPeo : urlLoc}></iframe>
+      <div id={'viz'} className={sty.neovis} style={{ width: "100%", height: "100%" }}></div>
       <div className={sty.Control} >
         <Select value={byWhat} onChange={(e) => setByWhat(e)}>
           <Select.Option value={"people"}> 按人群</Select.Option>
