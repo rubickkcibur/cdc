bd2bb94228c37277dfb7ee722c96844f0219abc9
Tarpelite <tarpelite_chan@foxmail.com>
2020-11-08 14:51:41 +0800

    overview switch and submit API swagger

--- fe/src/pages/overview/index.tsx
+++ fe/src/pages/overview/index.tsx
@@ -5,8 +5,11 @@ import sty from './index.module.scss'
 
 export default function PageOverview() {
   const [byWhat, setByWhat] = useState("people")
-  const draw = () => {
-    var config = {
+  function draw (e:string) {
+    var config;
+    setByWhat(e);
+    if (e == "location"){
+     config = {
       container_id: "viz",
       server_url: "bolt://123.57.0.181:7687",
       server_user: "neo4j",
@@ -25,20 +28,47 @@ export default function PageOverview() {
       },
       initial_cypher: "MATCH p=()-[r:transport]->() RETURN p LIMIT 25"
     };
+  }
+  else{
+     config = {
+      container_id: "viz",
+      server_url: "bolt://123.57.0.181:7687",
+      server_user: "neo4j",
+      server_password: "123456",
+      labels: {
+          "Patient":{
+            "size":"count",
+            "caption":"name",
+          },
+          "Contact":{
+              "size":"count",
+              "caption":"name"
+          }
+      },
+      relationships: {
+          "contact":{
+                        "caption":true,
+                        "thickness":"count"
+          }
+      },
+      initial_cypher: "MATCH p=()-[r:`contact`]->() RETURN p LIMIT 25"
+    };
+  }
+                     
     const NeoVis = require('neovis.js/dist/neovis')
     const viz = new NeoVis.default(config)
     viz.render();
   }
   useEffect(() => {
     if (process.browser)
-      draw()
+      draw("people")
   }, [])
 
   return <MainLayout>
     <div className={sty.FrameContainer}>
       <div id={'viz'} className={sty.neovis} style={{ width: "100%", height: "100%" }}></div>
       <div className={sty.Control} >
-        <Select value={byWhat} onChange={(e) => setByWhat(e)}>
+        <Select value={byWhat} onChange={(e) => draw(e)}>
           <Select.Option value={"people"}> 按人群</Select.Option>
           <Select.Option value={"location"}> 按地点</Select.Option>
         </Select>
